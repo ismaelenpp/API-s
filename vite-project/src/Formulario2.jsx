e48f84fd02158bd9b4ee0bb6d7db31e4454d8e72
ismaelenpp <ismaelgarciabougrine3@gmail.com>
2023-07-21 08:19:35 +0200

    nueva branca

--- vite-project/src/Formulario2.jsx
+++ vite-project/src/Formulario2.jsx
@@ -1,13 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Dropdown } from "react-bootstrap";
+import "./formulario2.css"; // Importar el archivo CSS creado
+import "bootstrap/dist/css/bootstrap.min.css"; // Importar estilos de Bootstrap
 
 const Formulario2 = ({ equipoSeleccionado, onEdit }) => {
+  const [paises, setPaises] = useState([]);
+  const [ligas, setLigas] = useState([]);
   const [equipo, setEquipo] = useState(equipoSeleccionado.nombre);
-  const [liga, setLiga] = useState(equipoSeleccionado.liga);
-  const [pais, setPais] = useState(equipoSeleccionado.pais);
+  const [selectedPais, setSelectedPais] = useState(equipoSeleccionado.pais);
+  const [selectedLiga, setSelectedLiga] = useState(equipoSeleccionado.liga);
   const [descripcion, setDescripcion] = useState(
     equipoSeleccionado.descripcion
   );
   const [imagen, setImagen] = useState(equipoSeleccionado.imagen);
+
+  useEffect(() => {
+    // Obtener la lista de países desde la API "Rest Countries"
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        const countries = response.data
+          .map((country) => country.name.common)
+          .sort();
+        setPaises(countries);
+      })
+      .catch((error) => {
+        console.error("Error fetching countries:", error);
+      });
+
+    // Obtener todas las ligas de fútbol desde la API "TheSportsDB"
+    axios
+      .get(
+        "https://apiclient.besoccerapps.com/scripts/api/api.php?key=9ce951e467376ca62e905f6b88a4e6de&tz=Europe/Madrid&req=categories&filter=my_leagues&format=json"
+      )
+      .then((response) => {
+        const leagues = response.data.category;
+        setLigas(leagues);
+      })
+      .catch((error) => {
+        console.error("Error fetching leagues:", error);
+      });
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,8 +49,8 @@ const Formulario2 = ({ equipoSeleccionado, onEdit }) => {
     const equipoEditado = {
       ...equipoSeleccionado,
       nombre: equipo,
-      liga: liga,
-      pais: pais,
+      liga: selectedLiga,
+      pais: selectedPais,
       descripcion: descripcion,
       imagen: imagen,
     };
@@ -25,8 +59,8 @@ const Formulario2 = ({ equipoSeleccionado, onEdit }) => {
 
     // Restablecer los campos del formulario
     setEquipo("");
-    setLiga("");
-    setPais("");
+    setSelectedPais("");
+    setSelectedLiga("");
     setDescripcion("");
     setImagen("");
   };
@@ -48,30 +82,46 @@ const Formulario2 = ({ equipoSeleccionado, onEdit }) => {
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="liga" className="form-label">
-            Liga
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="liga"
-            value={liga}
-            onChange={(e) => setLiga(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-3">
           <label htmlFor="pais" className="form-label">
             País
           </label>
-          <input
-            type="text"
-            className="form-control"
+          <select
+            className="form-select"
             id="pais"
-            value={pais}
-            onChange={(e) => setPais(e.target.value)}
+            value={selectedPais}
+            onChange={(e) => setSelectedPais(e.target.value)}
             required
-          />
+          >
+            <option value="" disabled>
+              Seleccionar País
+            </option>
+            {paises.map((pais) => (
+              <option key={pais} value={pais}>
+                {pais}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="liga" className="form-label">
+            Liga
+          </label>
+          <select
+            className="form-select"
+            id="liga"
+            value={selectedLiga}
+            onChange={(e) => setSelectedLiga(e.target.value)}
+            required
+          >
+            <option value="" disabled>
+              Seleccionar Liga
+            </option>
+            {ligas.map((liga) => (
+              <option key={liga.id} value={liga.shortName}>
+                {liga.shortName}
+              </option>
+            ))}
+          </select>
         </div>
         <div className="mb-3">
           <label htmlFor="descripcion" className="form-label">
