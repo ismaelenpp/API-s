4097d21b9b155e19aadf26a80290f5363aafb89f
ismaelenpp <ismaelgarciabougrine3@gmail.com>
2023-09-12 09:41:31 +0200

    Retiro

--- index.js
+++ index.js
@@ -1,86 +1,116 @@
 const express = require("express");
+
 const cors = require("cors");
+
 const app = express();
+
 const mysql = require("mysql2");
+
 const bodyParser = require("body-parser");
 
 // MySQL connection configuration
+
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "",
+
+  user: "ismael",
+
+  password: "ismaelenp1234",
+
   database: "futbol",
-  port: "3306"
+
+  port: "3306",
 });
 
 // Connect to MySQL
+
 connection.connect((err) => {
   if (err) {
     console.error("Error connecting to MySQL database:", err);
+
     return;
   }
+
   console.log("Connected to MySQL database");
 });
 
 var corsOptions = {
   origin: "http://localhost:5173",
+
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-cloudinary.config({ 
-  cloud_name: 'dajnd6hfe', 
-  api_key: '643243133882548', 
-  api_secret: 'Hqac499b90mUnZApKhIHUgpLCzc' 
-});
-
 app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // GET EQUIPO
+
 app.get("/futbol", (req, res) => {
   const query = "SELECT * FROM equipos";
 
   connection.query(query, (error, results) => {
     if (error) {
       console.error("Error executing MySQL query:", error);
+
       res.status(500).json({ error: "Internal Server Error" });
+
       return;
     }
 
     const equipos = results;
+
     res.status(200).json({ equipos });
   });
 });
 
 // POST EQUIPO
+
 app.post("/meterEquipo", (req, res) => {
   const { nombre, liga, pais, descripcion, imagen } = req.body;
+
   // Obtener la última ID
+
   const getLastIdQuery = "SELECT MAX(id) AS lastId FROM equipos";
+
   connection.query(getLastIdQuery, (error, result) => {
     if (error) {
       console.error("Error executing MySQL query:", error);
+
       res.status(500).json({ error: "Internal Server Error" });
+
       return;
     }
 
     let lastId = result[0].lastId || 0;
+
     const newId = lastId + 1;
+
     const query =
       "INSERT INTO `equipos`(`id`, `nombre`, `liga`, `pais`, `descripcion`, `imagen`) VALUES (?,?,?,?,?,?)";
+
     const values = [newId, nombre, liga, pais, descripcion, imagen];
+
     connection.query(query, values, (error, result) => {
       if (error) {
         console.error("Error executing MySQL query:", error);
+
         res.status(500).json({ error: "Internal Server Error" });
+
         return;
       }
+
       const newTeam = {
         id: newId,
+
         nombre,
+
         liga,
+
         pais,
+
         descripcion,
+
         imagen,
       };
 
@@ -90,22 +120,29 @@ app.post("/meterEquipo", (req, res) => {
 });
 
 // PUT DE EQUIPO
+
 app.put("/actualizarEquipo/:nombreEquipo", (req, res) => {
   const { nombre, liga, pais, descripcion, imagen } = req.body;
+
   const { nombreEquipo } = req.params;
+
   const query =
     "UPDATE equipos SET nombre = ?, liga = ?, pais = ?, descripcion = ?, imagen = ? WHERE nombre = ?";
+
   const values = [nombre, liga, pais, descripcion, imagen, nombreEquipo];
 
   connection.query(query, values, (error, result) => {
     if (error) {
       console.error("Error executing MySQL query:", error);
+
       res.status(500).json({ error: "Internal Server Error" });
+
       return;
     }
 
     if (result.affectedRows === 0) {
       res.status(404).json({ error: "Team not found" });
+
       return;
     }
 
@@ -114,19 +151,24 @@ app.put("/actualizarEquipo/:nombreEquipo", (req, res) => {
 });
 
 // DELETE EQUIPO
+
 app.delete("/eliminarEquipo/:nombreEquipo", (req, res) => {
   const { nombreEquipo } = req.params;
+
   const deleteQuery = "DELETE FROM equipos WHERE nombre = ?";
 
   connection.query(deleteQuery, nombreEquipo, (error, result) => {
     if (error) {
       console.error("Error executing MySQL query:", error);
+
       res.status(500).json({ error: "Internal Server Error" });
+
       return;
     }
 
     if (result.affectedRows === 0) {
       res.status(404).json({ error: "Team not found" });
+
       return;
     }
 
@@ -135,15 +177,16 @@ app.delete("/eliminarEquipo/:nombreEquipo", (req, res) => {
 });
 
 // Close the MySQL connection when the app is terminated
+
 process.on("SIGINT", () => {
   connection.end();
+
   process.exit();
 });
-
-console.log(prueba1());
-
 // Start the server
+
 const port = 3000;
+
 app.listen(port, () => {
   console.log(`API listening on port ${port}`);
 });
--- vite-project/src/Formulario.jsx
+++ vite-project/src/Formulario.jsx
@@ -1,21 +1,16 @@
 import React, { useState, useEffect } from "react";
-import DragAndDrop from "./drag_and_drop"; // Import the DragAndDrop component
-import cloudinary from "cloudinary-core";
-
-const cl = new cloudinary.Cloudinary({
-  cloud_name: 'dajnd6hfe', 
-  api_key: '643243133882548', 
-  api_secret: 'Hqac499b90mUnZApKhIHUgpLCzc',
-});
-
-
 
 import DragAndDrop from "./drag_and_drop";
+
 const Formulario = ({ onSubmit }) => {
   const [equipo, setEquipo] = useState("");
+
   const [liga, setLiga] = useState("");
+
   const [pais, setPais] = useState("");
+
   const [descripcion, setDescripcion] = useState("");
+
   const [imageFile, setImageFile] = useState(null);
 
   const [countries, setCountries] = useState([]); // Agregamos el estado para countries
@@ -57,6 +52,7 @@ const Formulario = ({ onSubmit }) => {
     setDescripcion("");
 
     setImageFile(null);
+    window.location.reload();
   };
 
   const handleImageDrop = (file) => {
@@ -72,43 +68,6 @@ const Formulario = ({ onSubmit }) => {
   const handleClearImage = () => {
     setImageFile(null);
   };
-
-const uploadImageToCloudinary = async (imageFile) => {
-  console.log("Subiendo imagen a Cloudinary...");
-  console.log(imageFile);
-  try {
-    const formData = new FormData();
-    formData.append("file", imageFile);
-    formData.append("upload_preset", "images"); // Reemplaza con tu upload preset
-
-    const response = await fetch(
-      cl.url("https://api.cloudinary.com/v1_1/dajnd6hfe/image/upload", {
-        secure: true,
-        upload_preset: "images",
-        cloud_name: "dajnd6hfe",
-        api_key: "643243133882548",
-        api_secret: "Hqac499b90mUnZApKhIHUgpLCzc",
-      }),
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
-    console.log(response)
-    if (response.ok) {
-      const data = await response.json();
-      console.log("Imagen cargada exitosamente:", data.secure_url);
-      // Puedes realizar cualquier acción adicional después de cargar la imagen aquí
-    } else {
-      // Manejo de errores si la respuesta no es exitosa
-      const errorData = await response.json();
-      console.error("Error al subir la imagen:", errorData.message);
-    }
-  } catch (error) {
-    console.error("Error al subir la imagen:", error);
-  }
-};
-
 
   return (
     <div className="container">
@@ -210,8 +169,8 @@ const uploadImageToCloudinary = async (imageFile) => {
             <DragAndDrop onImageDrop={handleImageDrop} />
           )}
         </div>
-        <button type="submit" className="btn btn-primary"
-        onClick={()=> uploadImageToCloudinary(imageFile)}>
+
+        <button type="submit" className="btn btn-primary">
           Añadir Equipo
         </button>
       </form>
