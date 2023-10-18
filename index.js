680bac533bf0c68c482963d055cb7a18ba2bd915
Daniel López Rodríguez <daniel.lopez-rodriguez@inetum.com>
2023-10-18 14:38:48 +0200

    control de errores hecho, falta quitar css

--- index.js
+++ index.js
@@ -1,157 +1,239 @@
 const express = require("express");
+
 const cors = require("cors");
+
 const app = express();
+
 const mysql = require("mysql2");
+
 const bodyParser = require("body-parser");
+
 const { enviartoken, sha256 } = require("./Funciones/funcionesUtiles");
+
 const filestack = require("filestack-js");
+
 const client = filestack.init("AZOIMYcHQJq6ZI7YPI0BEz");
+
 const ncrypt = require("ncrypt-js");
+
 const _secretKey = "some-super-secret-key";
+
 const ncryptObject = new ncrypt(_secretKey);
 
 app.use(bodyParser.json({ limit: "50mb" }));
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 // MySQL connection configuration
+
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "sergi",
-  password: "1234",
+
+  user: "root",
+
+  // password: "ismaelenp1234",
+
   database: "futbol",
+
   port: "3306",
 });
+
 // Connect to MySQL
+
 connection.connect((err) => {
   if (err) {
     console.error("Error connecting to MySQL database:", err);
+
     return;
   }
+
   console.log("Connected to MySQL database");
 });
 
 var corsOptions = {
   origin: "http://localhost:5173",
+
   optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // GET EQUIPO
+
 app.get("/futbol", (req, res) => {
   const query = "SELECT * FROM equipos";
+
   connection.query(query, (error, results) => {
     if (error) {
       console.error("Error executing MySQL query:", error);
+
       res.status(500).json({ error: "Internal Server Error" });
+
       return;
     }
+
     const equipos = results;
+
     res.status(200).json({ equipos });
   });
 });
 
 // POST EQUIPO
+
 app.post("/meterEquipo", (req, res) => {
   const { nombre, liga, pais, descripcion, imagen } = req.body;
+
   // Obtener la última ID
+
   const getLastIdQuery = "SELECT MAX(id) AS lastId FROM equipos";
+
   connection.query(getLastIdQuery, (error, result) => {
     if (error) {
       console.error("Error executing MySQL query:", error);
+
       res.status(500).json({ error: "Internal Server Error" });
+
       return;
     }
+
     let lastId = result[0].lastId || 0;
+
     const newId = lastId + 1;
+
     const query =
       "INSERT INTO `equipos`(`id`, `nombre`, `liga`, `pais`, `descripcion`, `imagen`) VALUES (?,?,?,?,?,?)";
+
     const values = [newId, nombre, liga, pais, descripcion, imagen];
+
     connection.query(query, values, (error, result) => {
       if (error) {
         console.error("Error executing MySQL query:", error);
+
         res.status(500).json({ error: "Internal Server Error" });
+
         return;
       }
+
       const newTeam = {
         id: newId,
+
         nombre,
+
         liga,
+
         pais,
+
         descripcion,
+
         imagen,
       };
+
       res.status(201).json(newTeam);
     });
   });
 });
 
 // PUT DE EQUIPO
+
 app.put("/actualizarEquipo/:nombreEquipo", (req, res) => {
   const { nombre, liga, pais, descripcion, imagen } = req.body;
+
   const { nombreEquipo } = req.params;
+
   const query =
     "UPDATE equipos SET nombre = ?, liga = ?, pais = ?, descripcion = ?, imagen = ? WHERE nombre = ?";
+
   const values = [nombre, liga, pais, descripcion, imagen, nombreEquipo];
+
   connection.query(query, values, (error, result) => {
     if (error) {
       console.error("Error executing MySQL query:", error);
+
       res.status(500).json({ error: "Internal Server Error" });
+
       return;
     }
+
     if (result.affectedRows === 0) {
       res.status(404).json({ error: "Team not found" });
+
       return;
     }
+
     res.status(200).json({ message: "Team updated successfully" });
   });
 });
 
 // DELETE EQUIPO
+
 app.delete("/eliminarEquipo/:nombreEquipo", (req, res) => {
   const { nombreEquipo } = req.params;
+
   const deleteQuery = "DELETE FROM equipos WHERE nombre = ?";
+
   connection.query(deleteQuery, nombreEquipo, (error, result) => {
     if (error) {
       console.error("Error executing MySQL query:", error);
+
       res.status(500).json({ error: "Internal Server Error" });
+
       return;
     }
+
     if (result.affectedRows === 0) {
       res.status(404).json({ error: "Team not found" });
+
       return;
     }
+
     // Aquí puedes realizar cualquier lógica adicional después de eliminar un equipo
+
     res.status(200).json({ message: "Team deleted successfully" });
   });
 });
 
 // Eliminar imagen de Cloudinary
+
 app.delete("/eliminarimagen/:eliminate", async (req, res) => {
   const { eliminate } = req.params;
+
   console.log(eliminate);
+
   const API_KEY = "AZOIMYcHQJq6ZI7YPI0BEz";
+
   const secretKey = "T5EHAT5TXZH6HJHUVBJRH5N6TE";
+
   const policyObject = {
     call: ["remove"],
+
     expiry: 1698795000,
+
     handle: eliminate,
   };
 
   const base64Policy = btoa(JSON.stringify(policyObject))
     .replace(/\+/g, "-")
+
     .replace(/\//g, "_");
+
   console.log("base64Policy", base64Policy);
+
   const policyAndKey = base64Policy + secretKey;
+
   //console.log("policyAndKey", policyAndKey);
+
   const signature = sha256(policyAndKey);
+
   console.log("signature", signature);
 
   try {
     const response = await fetch(
       `https://www.filestackapi.com/api/file/${eliminate}?key=${API_KEY}&policy=${base64Policy}&signature=${signature}`,
+
       {
         method: "DELETE",
       }
@@ -162,7 +244,9 @@ app.delete("/eliminarimagen/:eliminate", async (req, res) => {
     } else {
       console.error(
         "Error al eliminar la imagen:",
+
         response.status,
+
         response.statusText
       );
     }
@@ -172,102 +256,117 @@ app.delete("/eliminarimagen/:eliminate", async (req, res) => {
 });
 
 // POST para agregar un usuario a la tabla "usuarios"
+
 app.post("/meterGmail", async (req, res) => {
   const { correo } = req.body;
+
   let numeroAleatorio = Math.floor(Math.random() * 900000) + 100000;
 
   const numeroAleatorioCorto = numeroAleatorio.toString().substring(0, 6);
+
   let codigoEncriptado = ncryptObject.encrypt(numeroAleatorioCorto);
 
   console.log(correo);
+
   // Obtener la última ID
+
   console.log("correo", correo);
+
   const getLastIdQuery = "SELECT MAX(id) AS lastId FROM usuarios";
+
   connection.query(getLastIdQuery, (error, result) => {
     if (error) {
       console.error("Error executing MySQL query:", error);
+
       res.status(500).json({ error: "Internal Server Error" });
+
       return;
     }
+
     console.log("codigoEncriptado: " + codigoEncriptado);
+
     let lastId = result[0].lastId || 0;
+
     const newId = lastId + 1;
+
     const query =
       "INSERT INTO `usuarios`(`id`, `correo`, `codigo`, `rol`) VALUES (?,?,?,?)";
 
     const values = [newId, correo, codigoEncriptado, "usuario"];
+
     connection.query(query, values, (error, result) => {
       if (error) {
         console.error("Error executing MySQL query:", error);
+
         res.status(500).json({ error: "Internal Server Error" });
+
         return;
       }
+
       const newUser = {
         id: newId,
+
         correo,
+
         codigo: codigoEncriptado,
+
         rol: "usuario",
       };
+
       res.status(201).json(newUser);
     });
   });
+
   enviartoken(correo, codigoEncriptado);
 });
 
 app.post("/verificarCodigo", (req, res) => {
   const { codigo } = req.body;
-  console.log(codigo);
-  const codigo2 = ncryptObject.encrypt(codigo);
   const { email } = req.body;
-  const query = "SELECT * FROM usuarios WHERE correo = '" + email + "';";
-  const values = [codigo];
-  const values2 = [email];
-  const codigoEncriptado = ncryptObject.encrypt(codigo);
 
-  // console.log("codigo que pone el usuario:   " + codigoEncriptado);
   try {
-    connection.query(query, values, (error, result) => {
-      console.log(
-        result[0].codigo + "        vavava        " + codigoEncriptado
-      );
+    const codigo2 = ncryptObject.encrypt(codigo);
+    const query = "SELECT * FROM usuarios WHERE codigo = ? AND correo = ?";
+
+    connection.query(query, [codigo2, email], (error, result) => {
       if (error) {
-        console.log("1");
         console.error("Error executing MySQL query:", error);
         res.status(500).json({ error: "Internal Server Error" });
         return;
       }
 
       if (!result || result.length === 0) {
-        console.log("2");
-        res.status(404).json({ error: "User not found" });
+        console.log("Código incorrecto");
+        res.status(401).json({ error: "Token Incorrecto" });
         return;
       }
 
-      if (result[0].codigo == codigoEncriptado && result[0].correo == email) {
-        console.log("3");
+      if (result[0].codigo === codigo2 && result[0].correo === email) {
         console.log("El logueo es correcto");
         res.status(200).json({ message: "Token Correcto" });
       } else {
-        console.log("El logueo es INCORRECTO");
-        console.log("4");
+        console.log("Código incorrecto");
         res.status(401).json({ error: "Token Incorrecto" });
       }
     });
   } catch (error) {
-    console.log("5");
     console.error("Unexpected error:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Cerrar la conexión MySQL cuando la aplicación se termine
+
 process.on("SIGINT", () => {
   connection.end();
+
   process.exit();
 });
 
 // Iniciar el servidor
+
 const port = 3000;
+
 app.listen(port, () => {
   console.log(`API listening on port ${port}`);
 });
--- vite-project/src/login/pantallaCodigo.jsx
+++ vite-project/src/login/pantallaCodigo.jsx
@@ -1,10 +1,11 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useNavigate } from "react-router-dom";
+
 function PantallaCodigo() {
   const [codigo, setCodigo] = useState("");
+  const [codigoIncorrecto, setCodigoIncorrecto] = useState(false); // Nuevo estado
   const location = useLocation();
   const navigate = useNavigate();
   const { email } = location.state || {};
@@ -18,7 +19,6 @@ function PantallaCodigo() {
     console.log("codigo ---->", codigoIngresado);
     console.log("email ---->", emailIngresado);
 
-    // Promesa al servidor para verificar el código
     try {
       const response = await fetch("http://localhost:3000/verificarCodigo", {
         method: "POST",
@@ -35,72 +35,90 @@ function PantallaCodigo() {
         navigate("/app", { state: { email2: email } });
       } else {
         console.error("Error al verificar usuario");
+        setCodigoIncorrecto(true);
+
+        // Ocultar el mensaje después de 3 segundos
+        setTimeout(() => {
+          setCodigoIncorrecto(false);
+        }, 3000);
       }
     } catch (error) {
-      console.error("Error de red:", error);
+      // Manejo del error
     }
   };
+
   return (
-      <div className="background">
-        <section style={{ backgroundColor: "#004D7B" }}>
-          <div className="container py-5 h-100">
-            <div className="row d-flex justify-content-center align-items-center h-100">
-              <div className="col-12">
-                <div
-                    className="card bg-light text-white rounded"
-                    style={{ background: "url(https://cdn.filestackcontent.com/6MG9zFKMTfSapBgsCvvS)", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center"}}>
-                  <div className="card-body p-5 col text-center">
-                    <div className="col text-center">
-                      <img src="https://cdn.filestackcontent.com/tr0F3biuQEexN34fwBuy" class="rounded img-fluid w-75" alt="logo"/>
-                    </div>
-                    <div className="mb-md-5 mt-md-4 pb-5">
-                      <h6 className="fw-bold mb-2 text-uppercase text-black">Login</h6>
-                      <p className="text-black-50 mb-5">
-                        Introduce el codigo
-                      </p>
-                      <form onSubmit={handleSubmit}>
-                        <div className="form-outline form-white text-black mb-4">
-                          <label className="form-label" htmlFor="typeEmailX">
+    <div className="background">
+      <section style={{ backgroundColor: "#004D7B" }}>
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12">
+              <div
+                className="card bg-light text-white rounded"
+                style={{
+                  background:
+                    "url(https://cdn.filestackcontent.com/6MG9zFKMTfSapBgsCvvS)",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }}
+              >
+                <div className="card-body p-5 col text-center">
+                  <div className="col text-center">
+                    <img
+                      src="https://cdn.filestackcontent.com/tr0F3biuQEexN34fwBuy"
+                      className="rounded img-fluid w-75"
+                      alt="logo"
+                    />
+                  </div>
 
-                          </label>
-                          <input
-                              type="text"
-                              className="form-control"
-                              id="codigo"
-                              placeholder="Codigo"
-                              name="codigo"
-                              value={codigo}
-                              onChange={(e) => setCodigo(e.target.value)}
-                              required
-                          />
-                        </div>
-
-                        <button
-                            className="btn btn-outline-light  bg-primary btn-lg px-5 text-white"
-                            type="submit"
-                        >
-                          Login
-                        </button>
-                      </form>
-                      <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                        <a href="#!" className="text-white">
-                          <i className="fab fa-facebook-f fa-lg"></i>
-                        </a>
-                        <a href="#!" className="text-white">
-                          <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
-                        </a>
-                        <a href="#!" className="text-white">
-                          <i className="fab fa-google fa-lg"></i>
-                        </a>
+                  <div className="mb-md-5 mt-md-4 pb-5">
+                    <h6 className="fw-bold mb-2 text-uppercase text-black">Login</h6>
+                    <p className="text-black-50 mb-5">Introduce el codigo</p>
+                    {codigoIncorrecto && (
+                      <div className={`text-danger bold-text fade-out`}>Código incorrecto</div>
+                    )}
+                    <form onSubmit={handleSubmit}>
+                      <div className="form-outline form-white text-black mb-4">
+                        <label className="form-label" htmlFor="typeEmailX"></label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="codigo"
+                          placeholder="Codigo"
+                          name="codigo"
+                          value={codigo}
+                          onChange={(e) => setCodigo(e.target.value)}
+                          required
+                        />
                       </div>
+
+                      <button
+                        className="btn btn-outline-light  bg-primary btn-lg px-5 text-white"
+                        type="submit"
+                      >
+                        Login
+                      </button>
+                    </form>
+                    <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                      <a href="#!" className="text-white">
+                        <i className="fab fa-facebook-f fa-lg"></i>
+                      </a>
+                      <a href="#!" className="text-white">
+                        <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
+                      </a>
+                      <a href="#!" className="text-white">
+                        <i className="fab fa-google fa-lg"></i>
+                      </a>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
